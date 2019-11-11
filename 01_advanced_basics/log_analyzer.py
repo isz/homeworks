26b9c8e289915c44b78100d47eb71d80d3d4a5b5
Ilya Zvezdin <ilya.zvezdin@gmail.com>
2019-11-11 17:38:54 +0500

    changes due comments

--- 01_advanced_basics/log_analyzer.py
+++ 01_advanced_basics/log_analyzer.py
@@ -10,6 +10,7 @@ from string import Template
 import json
 import logging
 import sys
+import argparse
 
 # log_format ui_short '$remote_addr  $remote_user $http_x_real_ip [$time_local] "$request" '
 #                     '$status $body_bytes_sent "$http_referer" '
@@ -30,25 +31,21 @@ REQ_TIME_IDX = -1
 
 
 def get_config_file_from_com_line():
-    if len(sys.argv) >= 3:
-        for i in range(len(sys.argv)-1):
-            if sys.argv[i] == '--config':
-                return sys.argv[i+1]
+    parser = argparse.ArgumentParser(description='Log analyzer')
+    parser.add_argument('-c', '--config', type=str,
+                        help='config file in JSON format')
+    args = parser.parse_args()
 
-    return ''
+    return args.config
 
 
 def parse_config_file(config_file):
     try:
         with open(DEFAULT_CONFIG_PATH + config_file, mode='rt', encoding='utf-8') as f:
             conf = f.read()
-    except FileNotFoundError:
-        raise Exception(f'config file "{config_file}" not found')
-
-    try:
         conf = json.loads(conf)
-    except:
-        raise Exception(f'config "{config_file}" not in JSON format')
+    except Exception as e:
+        raise e
 
     if type(conf) is not dict:
         raise Exception(f'can\'t use "{config_file}" config file')
@@ -84,13 +81,13 @@ def parse_record(rec):
     try:
         url = fields[URL_IDX].strip('"')
         req_time = fields[REQ_TIME_IDX]
-    except IndexError:
-        raise Exception("can't parse log record")
-
-    try:
         req_time = float(req_time)
-    except ValueError:
-        raise Exception("cant't read request time")
+    except Exception as e:
+        raise e
+    # except IndexError:
+    #     raise Exception("can't parse log record")
+    # except ValueError:
+    #     raise Exception("cant't read request time")
 
     return (url, req_time)
 
@@ -150,15 +147,17 @@ def get_statistic(urls, sum_time, req_count):
 def find_log(path):
     last_name = ''
     last_date = None
+
     try:
-        _, _, file_list = next(os.walk(path))
-    except StopIteration:
-        raise Exception(f'log dir "{path}" does not exist')
+        file_list = os.listdir(path)
+    except Exception as e:
+        raise e
     else:
         for file_name in file_list:
-            if re.match(r'nginx-access-ui.log-\d{8}(.gz){,1}$', file_name):
-                log_date = datetime.strptime(
-                    re.findall(r'\d{8}', file_name)[0], '%Y%m%d')
+            matches = re.findall(
+                r'^nginx-access-ui.log-(\d{8})(.gz)?$', file_name)
+            if matches:
+                log_date = datetime.strptime(matches[0][0], '%Y%m%d')
                 if last_date is None or log_date > last_date:
                     last_date = log_date
                     last_name = file_name
@@ -180,74 +179,71 @@ def get_stats_list(stat_dict):
 
 
 def report_render(urls_stat, rep_name, rep_templ):
-    try:
-        with open(rep_templ, mode='rt', encoding='utf-8') as f:
-            templ_str = f.read()
-    except FileNotFoundError:
-        logging.error('report template "%s" does not exist', rep_templ)
-        return
+
+    with open(rep_templ, mode='rt', encoding='utf-8') as f:
+        templ_str = f.read()
 
     templ = Template(templ_str)
     rep_json = json.dumps(get_stats_list(urls_stat))
     report = templ.safe_substitute(table_json=rep_json)
 
+    os.makedirs(os.path.dirname(rep_name), exist_ok=True)
+
+    with open(rep_name, mode='wt', encoding='utf-8',) as f:
+        f.write(report)
+
+
+def main(config):
     try:
-        os.makedirs(os.path.dirname(rep_name), exist_ok=True)
-    except Exception as e:
-        logging.error("can't create report directory, exception: %s", str(e))
+        log_name, log_date = find_log(config['LOG_DIR'])
+    except:
+        logging.exception("can't find log file")
+        sys.exit("can't find log file")
+
+    if not log_name:
+        logging.info('log file not found')
+        sys.exit('log file not found')
+
+    logging.info('last log file: %s', log_name)
+
+    rep_name = os.path.join(
+        config['REPORT_DIR'], log_date.strftime('report-%Y.%m.%d.html'))
+
+    if report_exist(rep_name):
+        logging.info('report for "%s" exist', log_name)
+        return
+
+    reader = file_reader(os.path.join(config['LOG_DIR'], log_name))
+
+    urls_stat, sum_time, req_count, error_count = parse_log(reader)
+
+    if error_count/req_count > config['ERROR_RATE']:
+        logging.error('error rate exceeded %s value', config['ERROR_RATE'])
+        return
+
+    urls_stat = dict(sorted(urls_stat.items(), key=lambda t: t[1]['time_sum'], reverse=True)[
+        :config['REPORT_SIZE']])
+
+    get_statistic(urls_stat, sum_time, req_count)
 
     try:
-        with open(rep_name, mode='wt', encoding='utf-8',) as f:
-            f.write(report)
-    except Exception as e:
-        logging.error('error while writing report, exception: %s', str(e))
+        report_render(urls_stat, rep_name, config['REPORT_TEMPLATE'])
+    except:
+        logging.exception("can't render report")
+        sys.exit("can't render report")
+    else:
+        logging.info(f"report {rep_name} successfully created")
+        print(f"report {rep_name} successfully created")
 
 
-def main():
+if __name__ == "__main__":
     logging.basicConfig(filename=log_config['FILE'], format=log_config['FORMAT'],
                         level=log_config['LEVEL'], datefmt='%Y.%m.%d %H:%M:%S')
 
     try:
-        try:
-            config = get_config()
-        except Exception as e:
-            logging.error(str(e))
-            sys.exit("can't read config file")
+        config = get_config()
+    except:
+        logging.exception("can't read config file")
+        sys.exit("can't read config file")
 
-        try:
-            log_name, log_date = find_log(config['LOG_DIR'])
-        except Exception as e:
-            logging.error(str(e))
-            return
-
-        if not log_name:
-            logging.info('log file not found')
-            return
-
-        logging.info('last log file: %s', log_name)
-
-        rep_name = config['REPORT_DIR'] + '/' + \
-            log_date.strftime('report-%Y.%m.%d.html')
-        if report_exist(rep_name):
-            logging.info('report for "%s" exist', log_name)
-            return
-
-        reader = file_reader(config['LOG_DIR']+'/'+log_name)
-        urls_stat, sum_time, req_count, error_count = parse_log(reader)
-
-        if error_count/req_count > config['ERROR_RATE']:
-            logging.error('error rate exceeded %s value', config['ERROR_RATE'])
-            return
-
-        urls_stat = dict(sorted(urls_stat.items(), key=lambda t: t[1]['time_sum'], reverse=True)[
-            :config['REPORT_SIZE']])
-
-        get_statistic(urls_stat, sum_time, req_count)
-        report_render(urls_stat, rep_name, config['REPORT_TEMPLATE'])
-
-    except Exception as e:
-        logging.exception('unhandled exception')
-
-
-if __name__ == "__main__":
-    main()
+    main(config)
--- 01_advanced_basics/poker.py
+++ 01_advanced_basics/poker.py
@@ -17,7 +17,7 @@
 # (относительно значения, возвращаемого hand_rank)
 # "руку" из 5ти карт. Кроме прочего в данном варианте "рука"
 # может включать джокера. Джокеры могут заменить карту любой
-# масти и ранга того же цвета, в колоде два джокерва.
+# масти и ранга того же цвета, в колоде два джокера.
 # Черный джокер '?B' может быть использован в качестве треф
 # или пик любого ранга, красный джокер '?R' - в качестве черв и бубен
 # любого ранга.
@@ -26,11 +26,13 @@
 # Вам наверняка пригодится itertools.
 # Можно свободно определять свои функции и т.п.
 # -----------------
-
+import itertools
 
 def hand_rank(hand):
     """Возвращает значение определяющее ранг 'руки'"""
     ranks = card_ranks(hand)
+    print hand
+    print ranks
     if straight(ranks) and flush(hand):
         return (8, max(ranks))
     elif kind(4, ranks):
@@ -50,44 +52,73 @@ def hand_rank(hand):
     else:
         return (0, ranks)
 
+ranks = '23456789TJQKA'
 
 def card_ranks(hand):
     """Возвращает список рангов (его числовой эквивалент),
     отсортированный от большего к меньшему"""
-    return
+    num_ranks = []
+    for card in hand:
+        num_ranks.append(ranks.index(card[0]))
+    return sorted(num_ranks)
 
 
 def flush(hand):
     """Возвращает True, если все карты одной масти"""
-    return
+    hand_suits = set()
+    for card in hand:
+        hand_suits.add(card[1])
+    return len(hand_suits) == 1
 
 
 def straight(ranks):
     """Возвращает True, если отсортированные ранги формируют последовательность 5ти,
     где у 5ти карт ранги идут по порядку (стрит)"""
-    return
+    for i in range(len(ranks)-4):
+        if ranks[i:i+5] == [a for a in  range(ranks[i], ranks[i]+5)]:
+            return True
+    return False
 
 
 def kind(n, ranks):
     """Возвращает первый ранг, который n раз встречается в данной руке.
     Возвращает None, если ничего не найдено"""
-    return
+    # for rank in ranks:
+    #     if ranks.count(rank) == n:
+    #         return rank
+    for key, group in itertools.groupby(ranks, lambda i: i):
+        if len(list(group)) == n:
+            return key
+    return None
 
 
 def two_pair(ranks):
     """Если есть две пары, то возврщает два соответствующих ранга,
     иначе возвращает None"""
-    return
+    i = 0
+    pairs = []
+    while i < len(ranks)-1:
+        if ranks[i]==ranks[i+1]:
+            pairs.append(ranks[i])
+            i += 2
+        else:
+            i += 1
+        
+        if len(pairs) == 2:
+            return tuple(pairs)
+    return None
 
 
 def best_hand(hand):
     """Из "руки" в 7 карт возвращает лучшую "руку" в 5 карт """
-    return
 
+    print hand_rank(hand)
+    return hand
 
 def best_wild_hand(hand):
     """best_hand но с джокерами"""
-    return
+    print hand_rank(hand)
+    return hand
 
 
 def test_best_hand():
@@ -113,4 +144,4 @@ def test_best_wild_hand():
 
 if __name__ == '__main__':
     test_best_hand()
-    test_best_wild_hand()
+    # test_best_wild_hand()
