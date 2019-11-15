1fad9232cfc500190ea454fa913a7235b95113fa
Ilya Zvezdin <ilya.zvezdin@gmail.com>
2019-11-15 10:39:45 +0500

    typo fixed in directory name

---
+++ 01_advanced_basics/optional/poker.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# -----------------
+# Реализуйте функцию best_hand, которая принимает на вход
+# покерную "руку" (hand) из 7ми карт и возвращает лучшую
+# (относительно значения, возвращаемого hand_rank)
+# "руку" из 5ти карт. У каждой карты есть масть(suit) и
+# ранг(rank)
+# Масти: трефы(clubs, C), пики(spades, S), червы(hearts, H), бубны(diamonds, D)
+# Ранги: 2, 3, 4, 5, 6, 7, 8, 9, 10 (ten, T), валет (jack, J), дама (queen, Q), король (king, K), туз (ace, A)
+# Например: AS - туз пик (ace of spades), TH - дестяка черв (ten of hearts), 3C - тройка треф (three of clubs)
+
+# Задание со *
+# Реализуйте функцию best_wild_hand, которая принимает на вход
+# покерную "руку" (hand) из 7ми карт и возвращает лучшую
+# (относительно значения, возвращаемого hand_rank)
+# "руку" из 5ти карт. Кроме прочего в данном варианте "рука"
+# может включать джокера. Джокеры могут заменить карту любой
+# масти и ранга того же цвета, в колоде два джокера.
+# Черный джокер '?B' может быть использован в качестве треф
+# или пик любого ранга, красный джокер '?R' - в качестве черв и бубен
+# любого ранга.
+
+# Одна функция уже реализована, сигнатуры и описания других даны.
+# Вам наверняка пригодится itertools.
+# Можно свободно определять свои функции и т.п.
+# -----------------
+import itertools
+
+def hand_rank(hand):
+    """Возвращает значение определяющее ранг 'руки'"""
+    ranks = card_ranks(hand)
+    print hand
+    print ranks
+    if straight(ranks) and flush(hand):
+        return (8, max(ranks))
+    elif kind(4, ranks):
+        return (7, kind(4, ranks), kind(1, ranks))
+    elif kind(3, ranks) and kind(2, ranks):
+        return (6, kind(3, ranks), kind(2, ranks))
+    elif flush(hand):
+        return (5, ranks)
+    elif straight(ranks):
+        return (4, max(ranks))
+    elif kind(3, ranks):
+        return (3, kind(3, ranks), ranks)
+    elif two_pair(ranks):
+        return (2, two_pair(ranks), ranks)
+    elif kind(2, ranks):
+        return (1, kind(2, ranks), ranks)
+    else:
+        return (0, ranks)
+
+ranks = '23456789TJQKA'
+
+def card_ranks(hand):
+    """Возвращает список рангов (его числовой эквивалент),
+    отсортированный от большего к меньшему"""
+    num_ranks = []
+    for card in hand:
+        num_ranks.append(ranks.index(card[0]))
+    return sorted(num_ranks)
+
+
+def flush(hand):
+    """Возвращает True, если все карты одной масти"""
+    hand_suits = set()
+    for card in hand:
+        hand_suits.add(card[1])
+    return len(hand_suits) == 1
+
+
+def straight(ranks):
+    """Возвращает True, если отсортированные ранги формируют последовательность 5ти,
+    где у 5ти карт ранги идут по порядку (стрит)"""
+    for i in range(len(ranks)-4):
+        if ranks[i:i+5] == [a for a in  range(ranks[i], ranks[i]+5)]:
+            return True
+    return False
+
+
+def kind(n, ranks):
+    """Возвращает первый ранг, который n раз встречается в данной руке.
+    Возвращает None, если ничего не найдено"""
+    # for rank in ranks:
+    #     if ranks.count(rank) == n:
+    #         return rank
+    for key, group in itertools.groupby(ranks, lambda i: i):
+        if len(list(group)) == n:
+            return key
+    return None
+
+
+def two_pair(ranks):
+    """Если есть две пары, то возврщает два соответствующих ранга,
+    иначе возвращает None"""
+    i = 0
+    pairs = []
+    while i < len(ranks)-1:
+        if ranks[i]==ranks[i+1]:
+            pairs.append(ranks[i])
+            i += 2
+        else:
+            i += 1
+        
+        if len(pairs) == 2:
+            return tuple(pairs)
+    return None
+
+
+def best_hand(hand):
+    """Из "руки" в 7 карт возвращает лучшую "руку" в 5 карт """
+
+    print hand_rank(hand)
+    return hand
+
+def best_wild_hand(hand):
+    """best_hand но с джокерами"""
+    print hand_rank(hand)
+    return hand
+
+
+def test_best_hand():
+    print "test_best_hand..."
+    assert (sorted(best_hand("6C 7C 8C 9C TC 5C JS".split()))
+            == ['6C', '7C', '8C', '9C', 'TC'])
+    assert (sorted(best_hand("TD TC TH 7C 7D 8C 8S".split()))
+            == ['8C', '8S', 'TC', 'TD', 'TH'])
+    assert (sorted(best_hand("JD TC TH 7C 7D 7S 7H".split()))
+            == ['7C', '7D', '7H', '7S', 'JD'])
+    print 'OK'
+
+
+def test_best_wild_hand():
+    print "test_best_wild_hand..."
+    assert (sorted(best_wild_hand("6C 7C 8C 9C TC 5C ?B".split()))
+            == ['7C', '8C', '9C', 'JC', 'TC'])
+    assert (sorted(best_wild_hand("TD TC 5H 5C 7C ?R ?B".split()))
+            == ['7C', 'TC', 'TD', 'TH', 'TS'])
+    assert (sorted(best_wild_hand("JD TC TH 7C 7D 7S 7H".split()))
+            == ['7C', '7D', '7H', '7S', 'JD'])
+    print 'OK'
+
+if __name__ == '__main__':
+    test_best_hand()
+    # test_best_wild_hand()
